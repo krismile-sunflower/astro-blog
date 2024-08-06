d273b4b684938b7ce024000c7304d0681a72d1e9
zcc <892212481@qq.com>
2024-08-06 14:22:33 +0800

    fix: 处理部署后的wheater不能加载问题

--- src/pages/api/wheater.json.ts
+++ src/pages/api/wheater.json.ts
@@ -10,53 +10,69 @@ const remoteExecutablePath =
 
 export const GET: APIRoute = async ({ params, request }) => {
   const isDev = process.env.NODE_ENV === "development";
-  const browser = await puppeteer.launch({
-    args: isDev ? [] : chromium.args,
-    executablePath: isDev
-      ? localExecutablePath
-      : await chromium.executablePath(remoteExecutablePath),
-  });
-
-  const page = await browser.newPage();
-
-  await page.goto("https://tianqi.qq.com");
-
-  await page.waitForSelector("#ct-current");
-
-  const temperature = await page.$eval(
-    "#txt-temperature",
-    (el) => el.textContent
-  );
-  const status = await page.$eval("#txt-name", (el) => el.textContent);
-  const weather = await page.$eval(
-    "#ct-aqi > .info-aqi",
-    (el) => el.textContent
-  );
-  const text = await page.$eval("#txt-tips", (el) => el.textContent);
-  const wind = await page.$eval("#txt-wind", (el) => el.textContent);
-  const humidity = await page.$eval("#txt-humidity", (el) => el.textContent);
-  const address = await page.$eval("#txt-cur-location", (el) => el.textContent);
-
-  await browser.close();
-  return new Response(
-    JSON.stringify(
+  if (isDev) {
+    const browser = await puppeteer.launch({
+      args: isDev ? [] : chromium.args,
+      executablePath: isDev
+        ? localExecutablePath
+        : await chromium.executablePath(remoteExecutablePath),
+    });
+  
+    const page = await browser.newPage();
+  
+    await page.goto("https://tianqi.qq.com");
+  
+    await page.waitForSelector("#ct-current");
+  
+    const temperature = await page.$eval(
+      "#txt-temperature",
+      (el) => el.textContent
+    );
+    const status = await page.$eval("#txt-name", (el) => el.textContent);
+    const weather = await page.$eval(
+      "#ct-aqi > .info-aqi",
+      (el) => el.textContent
+    );
+    const text = await page.$eval("#txt-tips", (el) => el.textContent);
+    const wind = await page.$eval("#txt-wind", (el) => el.textContent);
+    const humidity = await page.$eval("#txt-humidity", (el) => el.textContent);
+    const address = await page.$eval("#txt-cur-location", (el) => el.textContent);
+  
+    await browser.close();
+    return new Response(
+      JSON.stringify(
+        {
+          address,
+          temperature,
+          status,
+          weather,
+          text,
+          wind,
+          humidity,
+        },
+        null,
+        2
+      ),
       {
-        address,
-        temperature,
-        status,
-        weather,
-        text,
-        wind,
-        humidity,
-      },
-      null,
-      2
-    ),
-    {
-      headers: {
-        "Content-Type": "application/json",
-      },
-      status: 200,
-    }
-  );
+        headers: {
+          "Content-Type": "application/json",
+        },
+        status: 200,
+      }
+    );
+    
+  } else {
+    return new Response(
+      JSON.stringify(
+        {},
+        null,
+        2
+      ),
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        status: 200,
+      })
+  }
 };
