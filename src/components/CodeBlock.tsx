45c06ac161e938e8c2e4bb5c3eb51b0340ccc357
zcc <892212481@qq.com>
2024-08-06 14:56:33 +0800

    feat: 處理代碼換行問題

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -16,18 +16,11 @@ const CodeBlock = (props: Props) => {
 
   useEffect(() => {
     const fetchHtml = async () => {
-      
+
       const html = await codeToHtml(code, {
         lang: language,
         theme: 'dracula',
-        decorations: [
-          {
-            start: { line: 1, character: 0 },
-            end: { line: 1, character: 11 },
-            properties: { class: 'highlighted-word' },
-            alwaysWrap: true
-          }
-        ]
+       
       });
       setHtml(html);
     };
@@ -43,14 +36,11 @@ const CodeBlock = (props: Props) => {
 
   return (
     <div className="relative cursor-pointer" onClick={() => copyToClipboard(code)} >
-      <button className="absolute font-thin right-5 top-2 text-sm">
+      <button className="absolute font-thin right-5 top-2 text-sm opacity-20">
         复制
       </button>
-      {isTip && <span className="absolute right-5 top-10 text-sm text-green-500">已复制!</span>}
-      <pre className="whitespace-pre-wrap break-words">
-        <code className="whitespace-pre-wrap break-words" dangerouslySetInnerHTML={{ __html: html }}>
-        </code>
-      </pre>
+      {isTip && <span className="absolute right-5 top-10 text-sm text-green-500 opacity-30">已复制!</span>}
+      <div className="whitespace-pre-wrap break-words" dangerouslySetInnerHTML={{ __html: html }} />
     </div>
   );
 };
